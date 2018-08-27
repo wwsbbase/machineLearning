c67973b0acca1dc6f97416df431e15ae039b3afe
D.J. Zou <bopy2008@163.com>
2018-08-27 16:16:40 +0800

    【修改】 1.解决内存不足的问题 2.加上时间点 3.加上远程sftp

--- second.py
+++ second.py
@@ -1,32 +1,45 @@
+print("start...............")
 
-import time 
+import time
 import pandas as pd                                                                                                                                
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import CountVectorizer
-    
-print("read_csv..............."+ time.asctime())
-df_train = pd.read_csv('./train_set.csv')
-df_test = pd.read_csv('./test_set.csv')
+from tqdm import tqdm
+
+# @execution_time
+def reader_pandas(file, chunkSize=100000, patitions=10 ** 4):
+    reader = pd.read_csv(file, iterator=True)
+    chunks = []
+    with tqdm(range(patitions), 'Reading ...') as t:
+        for _ in t:
+            try:
+                chunk = reader.get_chunk(chunkSize)
+                chunks.append(chunk)
+            except StopIteration:
+                break
+    return pd.concat(chunks, ignore_index=True)    
+print("read csv...............")
+df_train = reader_pandas('./train_set.csv')
+df_test = reader_pandas('./test_set.csv')
 df_train.drop(columns=['article','id'], inplace=True)
 df_test.drop(columns=['article'], inplace=True)
 
-print("CountVectorizer..............."+ time.asctime())
+print("CountVectorizer...............")
 vectorizer = CountVectorizer(ngram_range=(1,2), min_df=3, max_df=0.9, max_features=100000)
 vectorizer.fit(df_train['word_seg'])
 x_train = vectorizer.transform(df_train['word_seg'])
 x_test = vectorizer.transform(df_test['word_seg'])
 y_train = df_train['class']-1
 
-print("LogisticRegression..............."+ time.asctime())
+print("LogisticRegression...............")
 lg = LogisticRegression(C=4, dual=True)
 lg.fit(x_train, y_train)
 
-print("predict..............."+ time.asctime())
 y_test = lg.predict(x_test)
 
 df_test['class'] = y_test.tolist()
 df_test['class'] = df_test['class'] + 1
-print("result_to_csv..............."+ time.asctime())
+
 df_result = df_test.loc[:, ['id', 'class']]
 df_result.to_csv('./result.csv', index=False)
 
